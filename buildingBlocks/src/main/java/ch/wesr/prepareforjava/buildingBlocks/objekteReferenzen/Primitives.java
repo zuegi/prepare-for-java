c858cefaa0b840905185b7c6214ceda6f341c9bf
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2018-02-05 21:55:10 +0100

    zuegi - Objekt Referenzen und Primitive Typen * fehlerhafter Code auskommtentiert

--- buildingBlocks/src/main/java/ch/wesr/prepareforjava/buildingBlocks/objekteReferenzen/Primitives.java
+++ buildingBlocks/src/main/java/ch/wesr/prepareforjava/buildingBlocks/objekteReferenzen/Primitives.java
@@ -3,13 +3,13 @@ package ch.wesr.prepareforjava.buildingBlocks.objekteReferenzen;
 public class Primitives {
 
     int million = 1_000_000;
-    int million1 = _1_000_000;
-    int million1 = 1_000_000_;
+//    int million1 = _1_000_000;
+//    int million1 = 1_000_000_;
 
-    double notAtStart = _1000.00;
-    double notAtEnd = 1000.00_;
-    double notByDecimal = 1000_.00;
-    double notByDecimalAfter = 1000._00;
+//    double notAtStart = _1000.00;
+//    double notAtEnd = 1000.00_;
+//    double notByDecimal = 1000_.00;
+//    double notByDecimalAfter = 1000._00;
 
     double notByDecimalAfter = 1_00_0.0_0;
 
